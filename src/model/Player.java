c66013f42aee4cccf3a093f87210b3fb3b4d05bb
Kevin Mera <kamcak@hotmail.es>
2021-04-23 19:35:39 -0500

    csv reader work in progress

---
+++ src/model/Player.java
@@ -0,0 +1,98 @@
+package model;
+
+public class Player {
+    private String fullName;
+    private int age;
+    private String team;
+    private double pointsPerGame;
+    private double reboundsPerGame;
+    private double assistsPerGame;
+    private double robberiesPerGame;
+    private double blocksPerGame;
+    private double generalEvaluation;
+    
+	public Player(String fullName, int age, String team, double pointsPerGame, double reboundsPerGame,
+			double assistsPerGame, double robberiesPerGame, double blocksPerGame, double generalEvaluation) {
+		this.fullName = fullName;
+		this.age = age;
+		this.team = team;
+		this.pointsPerGame = pointsPerGame;
+		this.reboundsPerGame = reboundsPerGame;
+		this.assistsPerGame = assistsPerGame;
+		this.robberiesPerGame = robberiesPerGame;
+		this.blocksPerGame = blocksPerGame;
+		this.generalEvaluation = generalEvaluation;
+	}
+	
+	public String getFullName() {
+		return fullName;
+	}
+	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
+	
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	
+	public double getPointsPerGame() {
+		return pointsPerGame;
+	}
+	
+	public void setPointsPerGame(double pointsPerGame) {
+		this.pointsPerGame = pointsPerGame;
+	}
+	
+	public double getReboundsPerGame() {
+		return reboundsPerGame;
+	}
+	
+	public void setReboundsPerGame(double reboundsPerGame) {
+		this.reboundsPerGame = reboundsPerGame;
+	}
+	
+	public double getAssistsPerGame() {
+		return assistsPerGame;
+	}
+	
+	public void setAssistsPerGame(double assistsPerGame) {
+		this.assistsPerGame = assistsPerGame;
+	}
+	
+	public double getRobberiesPerGame() {
+		return robberiesPerGame;
+	}
+	
+	public void setRobberiesPerGame(double robberiesPerGame) {
+		this.robberiesPerGame = robberiesPerGame;
+	}
+	
+	public double getBlocksPerGame() {
+		return blocksPerGame;
+	}
+	
+	public void setBlocksPerGame(double blocksPerGame) {
+		this.blocksPerGame = blocksPerGame;
+	}
+	
+	public double getGeneralEvaluation() {
+		return generalEvaluation;
+	}
+	
+	public void setGeneralEvaluation(double generalEvaluation) {
+		this.generalEvaluation = generalEvaluation;
+	}
+}
--- src/model/QuickBasketManager.java
+++ src/model/QuickBasketManager.java
@@ -1,5 +1,26 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class QuickBasketManager {
 
+	private final static String SEPARATOR = ",";
+	public final static String SAVE_PATH_FILE = "data/FBAdata.csv";
+	
+
+	public void importData() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(SAVE_PATH_FILE));
+		br.readLine();
+		String line = br.readLine();
+		while(line!=null) {
+			String[] parts = line.split(SEPARATOR);
+			String name = parts[0];
+			String nit = parts[1];
+			addRestaurant(name,nit,manager);
+			line = br.readLine();
+		}
+		br.close();
+	}
 }
