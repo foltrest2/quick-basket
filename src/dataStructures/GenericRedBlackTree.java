ac5efba3e33fb2b6ae1f69b93fd7c5380f953779
Alejandro Coronel Sanchez <alejo.co9@hotmail.com>
2021-04-24 23:25:48 -0500

    something going wrong while pulling from kevin branch

--- src/dataStructures/GenericRedBlackTree.java
+++ src/dataStructures/GenericRedBlackTree.java
@@ -3,13 +3,20 @@ package dataStructures;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GenericRedBlackTree<K extends Comparable<K>, V> {
+public class GenericRedBlackTree<K extends Comparable<K>, V> implements GenericRedBlackTreeInterface<K,V>{
 
-	// Root initialized to null.
 	private Node<K,V> root = null;
 
-	public GenericRedBlackTree() {}
-
+	/**
+	 * Constructor of an empty red black tree
+	 */
+	public GenericRedBlackTree() {
+		
+	}
+	/**
+	 * This method rotates a node to the left considering all the cases
+	 * @param x is the node to rotate
+	 */
 	private void leftRotate(Node<K,V> x){
 		leftRotateFixup(x);
 		Node<K,V> y;
@@ -27,279 +34,216 @@ public class GenericRedBlackTree<K extends Comparable<K>, V> {
 		y.setLeft(x);
 		x.setFather(y);
 	}
-
+	/**
+	 * This method correct the quantity of nodes in each subtree
+	 * @param x is the node to correct
+	 */
 	private void leftRotateFixup(Node<K,V> x){
-
-		// Case 1: Only x, x.getRight() and x.getRight().right always are not null.
-		if (isnull(x.getLeft()) && isnull(x.getRight().left)){
-			x.numLeft = 0;
-			x.numRight = 0;
-			x.getRight().numLeft = 1;
+		if (isnull(x.getLeft()) && isnull(x.getRight().getLeft())){
+			x.setNumLeft(0);
+			x.setNumRight(0);
+			x.getRight().setNumLeft(1);
 		}
-
-		// Case 2: x.getRight().left also exists in addition to Case 1
-		else if (isnull(x.getLeft()) && !isnull(x.getRight().left)){
-			x.numLeft = 0;
-			x.numRight = 1 + x.getRight().left.numLeft +
-					x.getRight().left.numRight;
-			x.getRight().numLeft = 2 + x.getRight().left.numLeft +
-					x.getRight().left.numRight;
+		else if (isnull(x.getLeft()) && !isnull(x.getRight().getLeft())){
+			x.setNumLeft(0);
+			x.setNumRight(1 + x.getRight().getLeft().getNumLeft() + x.getRight().getLeft().getNumRight());
+			x.getRight().setNumLeft(2 + x.getRight().getLeft().getNumLeft() + x.getRight().getLeft().getNumRight());
 		}
-
-		// Case 3: x.getLeft() also exists in addition to Case 1
-		else if (!isnull(x.getLeft()) && isnull(x.getRight().left)){
-			x.numRight = 0;
-			x.getRight().numLeft = 2 + x.getLeft().numLeft + x.getLeft().numRight;
-
+		else if (!isnull(x.getLeft()) && isnull(x.getRight().getLeft())){
+			x.setNumRight(0);
+			x.getRight().setNumLeft(2 + x.getLeft().getNumLeft() + x.getLeft().getNumRight());
 		}
-		// Case 4: x.getLeft() and x.getRight().left both exist in addtion to Case 1
 		else{
-			x.numRight = 1 + x.getRight().left.numLeft +
-					x.getRight().left.numRight;
-			x.getRight().numLeft = 3 + x.getLeft().numLeft + x.getLeft().numRight +
-					x.getRight().left.numLeft + x.getRight().left.numRight;
+			x.setNumRight(1 + x.getRight().getLeft().getNumLeft() + x.getRight().getLeft().getNumRight());
+			x.getRight().setNumLeft(3 + x.getLeft().getNumLeft() + x.getLeft().getNumRight() + x.getRight().getLeft().getNumLeft() + x.getRight().getLeft().getNumRight());
 		}
-
-	}// end leftRotateFixup(Node x)
-
+	}
+	/**
+	 * This method rotates a node to the right considering all the cases
+	 * @param x is the node to rotate
+	 */
 	private void rightRotate(Node<K,V> y){
 		rightRotateFixup(y);
-		Node<K,V> x = y.left;
-		y.left = x.getRight();
-
+		Node<K,V> x = y.getLeft();
+		y.setLeft(x.getRight());
 		if (!isnull(x.getRight()))
-			x.getRight().father = y;
-		x.father = y.father;
-
-		if (isnull(y.father))
+			x.getRight().setFather(y);
+		x.setFather(y.getFather());
+		if (isnull(y.getFather()))
 			root = x;
-
-		else if (y.father.right == y)
-			y.father.right = x;
-
+		else if (y.getFather().getRight() == y)
+			y.getFather().setRight(x);
 		else
-			y.father.left = x;
-		x.right = y;
-
-		y.father = x;
-
+			y.getFather().setLeft(x);
+		x.setRight(y);
+		y.setFather(x);
 	}
-
+	/**
+	 * This method correct the quantity of nodes in each subtree
+	 * @param x is the node to correct
+	 */
 	private void rightRotateFixup(Node<K,V> y){
-
-		if (isnull(y.right) && isnull(y.left.right)){
-			y.numRight = 0;
-			y.numLeft = 0;
-			y.left.numRight = 1;
+		if (isnull(y.getRight()) && isnull(y.getLeft().getRight())){
+			y.setNumRight(0);
+			y.setNumLeft(0);
+			y.getLeft().setNumRight(1);
 		}
-
-		else if (isnull(y.right) && !isnull(y.left.right)){
-			y.numRight = 0;
-			y.numLeft = 1 + y.left.right.numRight +
-					y.left.right.numLeft;
-			y.left.numRight = 2 + y.left.right.numRight +
-					y.left.right.numLeft;
+		else if (isnull(y.getRight()) && !isnull(y.getLeft().getRight())){
+			y.setNumRight(0);
+			y.setNumLeft(1 + y.getLeft().getRight().getNumRight() + y.getLeft().getRight().getNumLeft());
+			y.getLeft().setNumRight(2 + y.getLeft().getRight().getNumRight() +	y.getLeft().getRight().getNumLeft());
 		}
-
-		else if (!isnull(y.right) && isnull(y.left.right)){
-			y.numLeft = 0;
-			y.left.numRight = 2 + y.right.numRight +y.right.numLeft;
+		else if (!isnull(y.getRight()) && isnull(y.getLeft().getRight())){
+			y.setNumLeft(0);
+			y.getLeft().setNumRight(2 + y.getRight().getNumRight() +y.getRight().getNumLeft());
 		}
-
 		else{
-			y.numLeft = 1 + y.left.right.numRight +
-					y.left.right.numLeft;
-			y.left.numRight = 3 + y.right.numRight +
-					y.right.numLeft +
-					y.left.right.numRight + y.left.right.numLeft;
+			y.setNumLeft(1 + y.getLeft().getRight().getNumRight() + y.getLeft().getRight().getNumLeft());
+			y.getLeft().setNumRight(3 + y.getRight().getNumRight() + y.getRight().getNumLeft() + y.getLeft().getRight().getNumRight() 
+					+ y.getLeft().getRight().getNumLeft());
 		}
-
 	}
-
+	/**
+	 * This method insert a node into the tree
+	 * @param key is the key
+	 * @param value is the value
+	 */
 	public void insert(K key, V value) {
 		insert(new Node<K,V>(key, value), value);
 	}
-
+	/**
+	 * This method creates the links between the node to insert and its respective father
+	 * @param z node to insert
+	 * @param value value of the node
+	 * @return
+	 */
 	private void insert(Node<K,V> z, V value) {
-
 		Node<K,V> y = null;
 		Node<K,V> x = root;
-
-		// While we haven't reached a the end of the tree keep
-		// tryint to figure out where z should go
 		while (!isnull(x)){
 			y = x;
-
-			// if z.key is < than the current key, go left
-			if (z.key.compareTo(x.key) < 0){
-
-				// Update x.numLeft as z is < than x
-				x.numLeft++;
-				x = x.getLeft();
-			}
-
-			// else z.key >= x.key so go right.
-			else if(z.key.compareTo(x.key) > 0){
-
-				// Update x.numGreater as z is => x
-				x.numRight++;
-				x = x.getRight();
-			}
-			else {
-				x.values.add(value);
+			if (contains(z.getKey()) != null) {
+				contains(z.getKey()).getValues().add(value);
 				x = null;
 				return;
 			}
+			else if (z.getKey().compareTo(x.getKey()) < 0){
+				x.setNumLeft(x.getNumLeft()+1);
+				x = x.getLeft();
+			}
+			else if(z.getKey().compareTo(x.getKey()) > 0){
+				x.setNumRight(x.getNumRight()+1);
+				x = x.getRight();
+			}
 		}
-		// y will hold z's father
-		z.father = y;
-
-		// Depending on the value of y.key, put z as the left or
-		// right child of y
+		z.setFather(y);
 		if (isnull(y))
 			root = z;
-		else if (z.key.compareTo(y.key) < 0)
-			y.left = z;
+		else if (z.getKey().compareTo(y.getKey()) < 0)
+			y.setLeft(z);
 		else
-			y.right = z;
-
-		// Initialize z's children to null and z's color to red
-		z.left = null;
-		z.right = null;
-		z.color = Node.RED;
-
-		// Call insertFixup(z)
+			y.setRight(z);
+		z.setLeft(null);
+		z.setRight(null);
+		z.setColor(Node.RED);
 		insertFixup(z);
-
 	}
-
+	/**
+	 * This method fixes the colors to conserve the properties of the tree
+	 * @param z node added to fix colors of his parents
+	 */
 	private void insertFixup(Node<K,V> z){
-
 		Node<K,V> y = null;
-		// While there is a violation of the RedBlackTree properties..
-		if (z.father != null) {
-			while ((z.father != null)&&(z.father.color == Node.RED)){
-				// If z's father is the the left child of it's father.
-				if (z.father.father != null) {
-					if (z.father == z.father.father.left){
-
-						// Initialize y to z 's cousin
-						y = z.father.father.right;
-
-						// Case 1: if y is red...recolor
+		if (z.getFather() != null) {
+			while ((z.getFather() != null)&&(z.getFather().getColor() == Node.RED)){
+				if (z.getFather().getFather() != null) {
+					if (z.getFather() == z.getFather().getFather().getLeft()){
+						y = z.getFather().getFather().getRight();
 						if (y != null) {
-							if (y.color == Node.RED){
-								z.father.color = Node.BLACK;
-								y.color = Node.BLACK;
-								z.father.father.color = Node.RED;
-								z = z.father.father;
+							if (y.getColor() == Node.RED){
+								z.getFather().setColor(Node.BLACK);
+								y.setColor(Node.BLACK);
+								z.getFather().getFather().setColor(Node.RED);
+								z = z.getFather().getFather();
 							}
 						}
-						// Case 2: if y is black & z is a right child
-						else if (z == z.father.right){
-
-							// leftRotaet around z's father
-							z = z.father;
+						else if (z == z.getFather().getRight()){
+							z = z.getFather();
 							leftRotate(z);
-							z = z.father.father;
+							z = z.getFather().getFather();
 							rightRotate(z);
 						}
-
-						// Case 3: else y is black & z is a left child
 						else{
-							// recolor and rotate round z's grandpa
-							z.father.color = Node.BLACK;
-							z.father.father.color = Node.RED;
-							rightRotate(z.father.father);
+							z.getFather().setColor(Node.BLACK);
+							z.getFather().getFather().setColor(Node.RED);
+							rightRotate(z.getFather().getFather());
 						}
 					}
-					// If z's father is the right child of it's father.
 					else{
-						// Initialize y to z's cousin
-						y = z.father.father.left;
-
-						// Case 1: if y is red...recolor
+						y = z.getFather().getFather().getLeft();
 						if (y != null) {
-							if (y.color == Node.RED){
-								z.father.color = Node.BLACK;
-								y.color = Node.BLACK;
-								z.father.father.color = Node.RED;
-								z = z.father.father;
+							if (y.getColor() == Node.RED){
+								z.getFather().setColor(Node.BLACK);
+								y.setColor(Node.BLACK);
+								z.getFather().getFather().setColor(Node.RED);
+								z = z.getFather().getFather();
 							}
 						}
-						// Case 2: if y is black and z is a left child
-						else if (z == z.father.left){
+						else if (z == z.getFather().getLeft()){
 							// rightRotate around z's father
-							z = z.father;
+							z = z.getFather();
 							rightRotate(z);
-							z = z.father.father;
+							z = z.getFather().getFather();
 							leftRotate(z);
 						}
-						// Case 3: if y  is black and z is a right child
 						else{
-							// recolor and rotate around z's grandpa
-							z.father.color = Node.BLACK;
-							z.father.father.color = Node.RED;
-							leftRotate(z.father.father);
+							z.getFather().setColor(Node.BLACK);
+							z.getFather().getFather().setColor(Node.RED);
+							leftRotate(z.getFather().getFather());
 						}
 					}
 				}
-				if (z.father != null) {
-					if (z.father.color == Node.RED) {
-						z.father.color = Node.BLACK;
+				if (z.getFather() != null) {
+					if (z.getFather().getColor() == Node.RED) {
+						z.getFather().setColor(Node.BLACK);
 					}
 				}
-
 			}
-
-
-			// Color root black at all times
-			root.color = Node.BLACK;
+			root.setColor(Node.BLACK);
 		}
-	}// end insertFixup(Node z)
-
-	public Node<K,V> treeMinimum(Node<K,V> node){
-		while (!isnull(node.left))
-			node = node.left;
-		return node;
 	}
-
-	public Node<K,V> treeSuccessor(Node<K,V> x){
-
-		// if x.getLeft() is not null, call treeMinimum(x.getRight()) and
-		// return it's value
-		if (!isnull(x.getLeft()) )
-			return treeMinimum(x.getRight());
-
-		Node<K,V> y = x.getFather();
-
-		// while x is it's father's right child...
-		while (!isnull(y) && x == y.right){
-			// Keep moving up in the tree
-			x = y;
-			y = y.father;
-		}
-		// Return successor
-		return y;
-	}// end treeMinimum(Node x)
-
-	// @param: key, the key whose node we want to search for
-	// @return: returns a node with the key, key, if not found, returns null
-	// Searches for a node with key k and returns the first such node, if no
-	// such node is found returns null
+	/**
+	 * This method searches a node and returns its values
+	 * @param key is the key of the node searched
+	 */
 	public List<V> search(K key){
 		Node<K,V> current = root;
 		while (!isnull(current)){
-			if (current.key.equals(key))
-				return current.values;
-			else if (current.key.compareTo(key) < 0)
-				current = current.right;
+			if (current.getKey().equals(key))
+				return current.getValues();
+			else if (current.getKey().compareTo(key) < 0)
+				current = current.getRight();
 			else
-				current = current.left;
+				current = current.getLeft();
 		}
 		return null;
 	}
-	
+	/**
+	 * This method searches a node and returns the node
+	 * @param key is the key of the node searched
+	 */
+	private Node<K,V> contains(K key){
+		Node<K,V> current = root;
+		while (!isnull(current)){
+			if (current.getKey().equals(key))
+				return current;
+			else if (current.getKey().compareTo(key) < 0)
+				current = current.getRight();
+			else
+				current = current.getLeft();
+		}
+		return null;
+	}
 	/**
 	 * Returns sorted list of keys greater than key.  Size of list
 	 * will not exceed maxReturned
@@ -312,65 +256,81 @@ public class GenericRedBlackTree<K extends Comparable<K>, V> {
 		getGreaterThan(root, key, list);
 		return list.subList(0, Math.min(maxReturned, list.size()));
 	}
-
-	private void getGreaterThan(Node<K,V> node, K key, List<V> list) {
-		if (isnull(node)) {
+	/**
+	 * This method iterates on the tree to find the information required from the nodes
+	 * @param current actual node
+	 * @param key key of the node given by condition
+	 * @param list list to fill with the required elements
+	 */
+	private void getGreaterThan(Node<K,V> current, K key, List<V> list) {
+		if (isnull(current)) {
 			return;
-		} else if (node.key.compareTo(key) > 0) {
-			getGreaterThan(node.left, key, list);
-			list.addAll(node.values);
-			getGreaterThan(node.right, key, list);
+		} else if (current.getKey().compareTo(key) > 0) {
+			getGreaterThan(current.getLeft(), key, list);
+			list.addAll(current.getValues());
+			getGreaterThan(current.getRight(), key, list);
 		} else {
-			getGreaterThan(node.right, key, list);
+			getGreaterThan(current.getRight(), key, list);
 		}
 	}
-	
+	/**
+	 * Returns sorted list of keys lower than key.  Size of list
+	 * will not exceed maxReturned
+	 * @param key Key to search for
+	 * @param maxReturned Maximum number of results to return
+	 * @return List of keys lower than key.  List may not exceed maxReturned
+	 */
 	public List<V> getLowestThan(K key, Integer maxReturned) {
 		List<V> list = new ArrayList<>();
 		getLowestThan(root, key, list);
 		return list.subList(0, Math.min(maxReturned, list.size()));
 	}
-
-	private void getLowestThan(Node<K,V> node, K key, List<V> list) {
-		if (isnull(node)) {
+	/**
+	 * This method iterates on the tree to find the information required from the nodes
+	 * @param current actual node
+	 * @param key key of the node given by condition
+	 * @param list list to fill with the required elements
+	 */
+	private void getLowestThan(Node<K,V> current, K key, List<V> list) {
+		if (isnull(current)) {
 			return;
-		} else if (node.key.compareTo(key) < 0) {
-			getLowestThan(node.right, key, list);
-			list.addAll(node.values);
-			getLowestThan(node.left, key, list);
+		} else if (current.getKey().compareTo(key) < 0) {
+			getLowestThan(current.getRight(), key, list);
+			list.addAll(current.getValues());
+			getLowestThan(current.getLeft(), key, list);
 		} else {
-			getLowestThan(node.left, key, list);
+			getLowestThan(current.getLeft(), key, list);
 		}
 	}
-
-	public int findNumSmaller(Node<K,V> node, K key){
-		if (isnull(node)) return 0;
-		else if (key.compareTo(node.key) <= 0)
-			return findNumSmaller(node.left,key);
-		else
-			return 1+ node.numLeft + findNumSmaller(node.right,key);
+	/**
+	 * This method runs the tree and gets all the information from it
+	 */
+	public String preOrder() {
+		return preOrderRecursive(this.root).trim();
 	}
-
+	/**
+	 * This method iterates through all the tree getting the information from all nodes
+	 * @param root is the first node
+	 * @return String with all the information
+	 */
+	private String preOrderRecursive(Node<K, V> root) {
+		if (root == null) {
+			return "";
+		}
+		String s = "";
+		s += " " + root.getValues().toString();
+		s += preOrderRecursive(root.getLeft());
+		s += preOrderRecursive(root.getRight());
+		return s;	
+	}
 	private boolean isnull(Node<K,V> node){
 		return node == null;		
 	}
-
-	public int size(){
-		return root.numLeft + root.numRight + 1;
+	public Node<K,V> getRoot(){
+		return root;
 	}
+    public void reset() {
+    	root = null;
+    }
 
-    public String preOrder() {
-        return preOrderRecursive(this.root).trim();
-    }
-    
-    private String preOrderRecursive(Node<K, V> root) {
-    	if (root == null) {
-    		return "";
-    	}
-    	String s = "";
-    	s += " " + root.getValues().toString();
-    	s += preOrderRecursive(root.getLeft());
-    	s += preOrderRecursive(root.getRight());
-    	return s;	
-    }
 }
