92d35643420b03089f048f8ff8cfa84a0b0ecf3d
Alejandro Coronel Sanchez <alejo.co9@hotmail.com>
2021-04-24 23:40:36 -0500

    Almost finishing GUI

---
+++ src/dataStructures/GenericRedBlackTreeInterface.java
@@ -0,0 +1,12 @@
+package dataStructures;
+
+import java.util.List;
+
+public interface GenericRedBlackTreeInterface<K,V> {
+
+	public void insert(K key, V value);
+	public List<V> search(K key);
+	public List<V> getGreaterThan(K key, Integer maxReturned);
+	public List<V> getLowestThan(K key, Integer maxReturned);
+	
+}
